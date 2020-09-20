38096411b7831169307bdffc4fe97ca828ce0246
araujo ludovic <araujo.ludovic@gmail.com>
2020-09-20 22:26:05 +0200

    allocation cors origine

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ mongoose.connect('mongodb://localhost:27017/myNewDb',
 
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
+
+var cors = require('cors')
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
 app.use(body.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
