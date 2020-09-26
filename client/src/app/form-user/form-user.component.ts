5aeb5c68cb7b493a13d110b801f640363abd36a1
araujo ludovic <araujo.ludovic@gmail.com>
2020-09-26 17:13:01 +0200

    création user via formulaire

--- client/src/app/form-user/form-user.component.ts
+++ client/src/app/form-user/form-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UserService } from '../services/user.service';
 import {FormControl, Validators} from '@angular/forms';
 
 @Component({
@@ -11,14 +12,43 @@ export class FormUserComponent implements OnInit {
   @Output() loader = new EventEmitter<string>()
   hide = true;
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
   } 
 
+  onSubmit(event: any) {
+    var user = new Users
+
+    user.firstname = event.target.firstname.value
+    user.lastname = event.target.lastname.value
+    user.login = event.target.login.value
+    user.password = event.target.password.value
+
+    return this.newUser(user)
+  }
+
+  newUser(user : object){
+    this.userService.createUser(user).subscribe((value) => {
+        console.log(value);
+    }, (error) => {
+        console.log(error);
+    }, () => {
+        console.log('Fini !');
+    });
+  }
 
   changeOutput(){
     this.loader.emit()
   }
 
+}
+
+export class Users {
+  firstname: String;
+  lastname: String;
+  login: String;
+  password: String;
 }
