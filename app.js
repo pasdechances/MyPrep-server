3ffb661e61b72d7185042448b48b6383e007daed
araujo ludovic <araujo.ludovic@gmail.com>
2020-10-02 22:44:52 +0200

    Config Serveur

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-var cors = require('cors')
-mongoose.connect('mongodb://localhost:27017/myNewDb',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const config = JSON.parse(require('fs').readFileSync('./config.json'))
+const cors = require('cors')
+
+
 app.use(express.static(process.cwd()+"/client/dist/client/"));
 app.get('/', (req,res) => {
   res.sendFile(process.cwd()+"/client/dist/client/index.html")
 });
 
-var corsOptions = {
-    origin: 'http://localhost:4200',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
+var uri = config.cluster.connector+'://';
+if(config.cluster.userName){
+  uri += config.cluster.userName+':'+config.cluster.password+'@';
+}
+uri += config.cluster.url+':'+config.cluster.port+'/'+config.cluster.data;
+mongoose.connect(uri,config.cluster.call)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+var corsOptions = config.cors    //some legacy browsers (IE11, various SmartTVs) choke on optionsSuccessStatus : 204
 app.use(cors(corsOptions))
 
 const userRoutes = require('./routes/user');
