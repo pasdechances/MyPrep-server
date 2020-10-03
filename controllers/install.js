5facbd6051a28c060fe6c7288c13e90d0ca1330a
araujo ludovic <araujo.ludovic@gmail.com>
2020-10-03 22:51:06 +0200

    modif de l'install

--- controllers/install.js
+++ controllers/install.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const filePath = './config.json';
 const base = require('../resources/connect.js')
-const User = require('../models/user');
+
+const userCtrl = require('../controllers/user');
+const groupCtrl = require('../controllers/user');
+
 
 exports.login = (req, res, next) => {
     let config = JSON.parse(fs.readFileSync(filePath));
@@ -21,27 +24,56 @@ exports.login = (req, res, next) => {
     if(!req.body.user.name)return res.status(500).json({ field: 'name', error: 'field is empty' });
     if(!req.body.user.password)return res.status(500).json({ field: 'user', error: 'field is empty' });
 
-    const user = new User({
+    groupCtrl.createGroupe({
+        name: "Administrateur",
+        rights: {
+            userEditable: true,
+            groupEditable: true,
+            priceEditable: true,
+            stockUsable: true,
+            saleUsable: true,
+            paramUsable: true,
+            teletransmissionUsable: true,
+            vaccinationAllow: true,
+        } 
+    });
+
+    groupCtrl.createGroupe({
+        name: "Utilisateur",
+        rights: {
+            userEditable: false,
+            groupEditable: false,
+            priceEditable: false,
+            stockUsable: true,
+            saleUsable: true,
+            paramUsable: false,
+            teletransmissionUsable: false,
+            vaccinationAllow: false,
+        } 
+    });
+
+    userCtrl.createUser({
         firstname: "admin",
         lastname: "super",
         login: req.body.user.name,
         password: req.body.user.password,
-        });
-
-    user.save()
-    .then(() => {
-        console.log("SuperAdmin created")
-        next();
+        groups:["Administrateur"]
     })
-    .catch((error) => {
-        res.status(400).json({
-        error: error
-        });
-    });
+    .then(() => {console.log("User '" + user.login + "' created")})
+    .catch((error) => {console.log(error)})
     
     config.cluster = req.body.cluster;
     config.installed = true;
 
     fs.writeFileSync(filePath, JSON.stringify(config));
     return res.status(200).json({message : config});
-  };
+};
+
+
+function createGroupe(params){
+    const group = new Group(params);
+
+    group.save()
+    .then(() => {console.log("Group '" + group.name + "' created")})
+    .catch((error) => {console.log(error)});
+}
