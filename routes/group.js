ec9eea25aa62a313894f1ccfa0b3822457556016
ludo <ludo@ludo.fr>
2020-10-04 00:02:34 +0200

    routes pour les groupes

---
+++ routes/group.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const groupCtrl = require('../controllers/group');
+
+router.get('/', auth, groupCtrl.getAllGroups);
+router.get('/:id', auth, groupCtrl.getOneGroup);
+router.post('/', auth, groupCtrl.newGroup);
+router.put('/:id', auth, groupCtrl.modifyGroup);
+router.delete('/:id', auth, groupCtrl.deleteGroup);
+
+module.exports = router;
